99a3edeb598ac8e8aac79bfda105d10b93926432
myPC <fb.your.choose@gmail.com>
2019-03-16 16:38:27 +0200

    add new class for search max value in array

---
+++ Deltamodule/FindMaxValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deltamodule
+{
+    class FindMaxValue
+
+    {
+        public int findMax(int[] inArayy)
+        {
+
+
+
+            return 1;
+
+        }
+
+
+    }
+}
